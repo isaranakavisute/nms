fdc437b7423154b77b4959f13a8336251c916243
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-02-27 06:32:15 +0000

    userscreen separation

--- app/nms/layout.tsx
+++ app/nms/layout.tsx
@@ -1,6 +1,7 @@
 
-import user_screen from "./user_screen";
-import logo from "./images/logo.png";
+import Sidebar from "./sidebar.tsx";
+import UserScreen from "./userscreen.tsx";
+//import logo from "./images/new_logo.png";
 import Image from "next/image";
 
 export default function BackOfficeLayout({ children }: {
@@ -8,28 +9,10 @@ export default function BackOfficeLayout({ children }: {
 }) {
  return (
     <div className="flex flex-col">
-         <div className="flex h-[5vh] w-full items-center justify-center bg-blue-300 text-2xl">Pay Slip Sharing Tool</div>
-          <div className="flex h-[15vh] w-full flex-row border-solid border-2 items-center justify-between">
-           <div className="flex h-[15vh] flex-row items-center">
-            <div className="pr-10 pl-10">
-             <Image
-              src={logo}
-              alt="nms logo"
-             />
-            </div>
-            <div className="text-2xl">Welcome User Top Page</div>
-           </div>
-          <div className="flex h-[15vh] flex-row items-center justify-center pr-10">
-           <button className="text-2xl opacity-100 bg-blue-300 pl-20 pr-20 pt-5 pb-5 rounded-lg">Log Out</button>
-          </div>
-         </div>
-        <div className="h-[80vh] w-full bg-white flex flex-row items-center justify-center">
-         <div className="pr-20">
-          <button className="text-2xl opacity-100 bg-blue-300  p-20 rounded-xl">Check Slip</button>
-         </div>
-         <button className="text-2xl opacity-100 bg-blue-300  p-20 rounded-xl">History Search</button>
-        </div>
-       </div>
+
+        <Sidebar />
+
+    </div>
  );
 }
 
